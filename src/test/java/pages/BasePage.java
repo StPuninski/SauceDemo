b3e218bd2fa990afb4e94785cb68c609f3943e5c
StPuninski <stpuninski@gmail.com>
2020-04-24 21:47:52 +0300

    Добавил классную работу

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+public class BasePage {
+    WebDriver driver;
+    WebDriverWait wait;
+
+
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(this.driver,10);
+
+    }
+
+    public void waitForNumberOfElements(By locator, int numberOfElements) {
+        try {
+        wait.until(ExpectedConditions.numberOfElementsToBe(locator,numberOfElements));
+        } catch (TimeoutException ex){
+            ex.printStackTrace();
+            Assert.fail("Колличество элементов не соответствует ожидаемому числу" + numberOfElements);
+        }
+    }
+}
